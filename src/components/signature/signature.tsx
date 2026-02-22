0def90f304ada9747c21ef814afa74153a734adc
Fernando Maleiane <maleianefernando3@gmail.com>
2026-02-22 20:13:22 +0200

    fix: add target=_blank for contact links

--- src/components/signature/signature.tsx
+++ src/components/signature/signature.tsx
@@ -4,12 +4,12 @@ export const Signature = () => {
       <p className="text-sm text-slate-500">
         Desenvolvido por: <span className="font-bold">Fernando Maleiane</span>
       </p>
-      <div className="flex flex-col md:flex-row justify-between text-sky-500 px-4">
-        <a href="https://github.com/maleianefernando/">Github</a>
-        <a href="https://www.linkedin.com/in/fernando-maleiane-bb2078296/">
+      <div className="flex flex-col md:flex-row justify-between text-sky-500 px-4" >
+        <a href="https://github.com/maleianefernando/" target="_blank">Github</a>
+        <a href="https://www.linkedin.com/in/fernando-maleiane-bb2078296/" target="_blank">
           Linkedin
         </a>
-        <a href="mailto:maleianefernando3@gmail.com">
+        <a href="mailto:maleianefernando3@gmail.com" target="_blank">
           maleianefernando3@gmail.com
         </a>
         <div className="flex flex-col">
